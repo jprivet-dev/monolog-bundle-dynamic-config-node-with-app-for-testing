90561dab9ff157144eeb30176a177f3346820454
jprivet-dev <jprivet.dev@gmail.com>
2025-05-16 15:45:35 +0200

    feat(install.sh): create base of script

---
+++ scripts/install.sh
@@ -0,0 +1,22 @@
+# Usage
+# . scripts/install.sh
+set +x -e
+
+# Get the Symfony Source Code
+git clone git@github.com:jprivet-dev/symfony.git
+cd symfony
+git switch symfony-monolog-bundle-poc
+
+# Automatic tests
+composer update
+php ./phpunit src/Symfony/Bridge/Monolog
+
+# Creating a new test `app` project
+cd ..
+symfony new app --version=7.2
+cd app
+composer require symfony/monolog-bundle
+
+# Use the branch `jprivet-dev:symfony-monolog-bundle-poc` in the new `app` project
+cd ../symfony
+php link ../app
