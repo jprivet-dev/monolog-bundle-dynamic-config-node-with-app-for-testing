5e3a4e5005f482ccf5cd734c6276a646fd4eb377
jprivet-dev <jprivet.dev@gmail.com>
2025-06-06 18:01:41 +0200

    feat(poc-bundle/Configuration): complete examples

--- poc-bundle/src/DependencyInjection/Configuration.php
+++ poc-bundle/src/DependencyInjection/Configuration.php
@@ -2,6 +2,9 @@
 
 namespace Local\Bundle\PocBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+use Symfony\Component\Config\Definition\Builder\NodeBuilder;
+use Symfony\Component\Config\Definition\Builder\NodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
@@ -9,24 +12,73 @@ class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        //dump('PocBundle\Configuration::getConfigTreeBuilder');
         $treeBuilder = new TreeBuilder('poc');
         $rootNode = $treeBuilder->getRootNode();
 
         $rootNode
             ->children()
-                ->booleanNode('one')
+                ->booleanNode('a')
                     ->defaultTrue()
+                    ->info('info a')
                 ->end()
-                ->scalarNode('two')
+                ->scalarNode('b')
                     ->defaultValue('default')
+                    ->info('info b')
                 ->end()
-                ->arrayNode('three')
-                    ->canBeUnset()
-                    ->prototype('scalar')
-                ->end()
+                ->append(static::booleanNodeC())
+                ->append(static::arrayNodeD())
+                ->append(static::arrayNodeByName('e'))
+                ->append(static::arrayNodeByName('f', exclude: ['user', 'password']))
+                ->append(static::arrayNodeByName('g'))
             ->end();
 
         return $treeBuilder;
+    }
+
+    private static function booleanNodeC(): NodeDefinition {
+        return (new NodeBuilder())
+            ->booleanNode('c')
+                ->defaultTrue()
+                ->info('info c');
+    }
+
+    private static function arrayNodeD(): ArrayNodeDefinition {
+        return (new TreeBuilder('d'))->getRootNode()
+            ->info('info d')
+            ->children()
+                ->booleanNode('sub_d')
+                    ->defaultTrue()
+                    ->info('info sub_d')
+                ->end()
+            ->end();
+    }
+
+    private static function arrayNodeByName(string $name, array $exclude = []): ArrayNodeDefinition {
+        return (new TreeBuilder($name))->getRootNode()
+            ->info(sprintf('info %s', $name))
+            ->children()
+                ->scalarNode('id')
+                    ->info('info id')
+                ->end()
+                ->scalarNode('host')
+                    ->info('info host')
+                ->end()
+                ->scalarNode('port')
+                    ->defaultValue(9200)
+                    ->info('info port')
+                ->end()
+                ->scalarNode('transport')
+                    ->defaultValue('Http')
+                    ->info('info transport')
+                ->end()
+                ->scalarNode('user')
+                    ->defaultNull()
+                    ->info('info user')
+                ->end()
+                ->scalarNode('password')
+                    ->defaultNull()
+                    ->info('info password')
+                ->end()
+            ->end();
     }
 }
