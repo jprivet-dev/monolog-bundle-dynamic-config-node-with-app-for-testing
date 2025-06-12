eded0121514c3a2086d9dc6b5df02aa5eada7959
jprivet-dev <jprivet.dev@gmail.com>
2025-06-12 18:43:20 +0200

    feat(MonologPocBundle): HandlerConfiguration > adjust $node type

--- monolog-poc-bundle/src/DependencyInjection/HandlerConfiguration/HandlerConfiguration.php
+++ monolog-poc-bundle/src/DependencyInjection/HandlerConfiguration/HandlerConfiguration.php
@@ -4,13 +4,15 @@ namespace Local\Bundle\MonologPocBundle\DependencyInjection\HandlerConfiguration
 
 use Local\Bundle\MonologPocBundle\Definition\Builder\HandlerOptionsNodeDefinition;
 use Local\Bundle\MonologPocBundle\DependencyInjection\AddConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\NodeDefinition;
+use Symfony\Component\Config\Definition\Builder\VariableNodeDefinition;
 
 abstract class HandlerConfiguration implements AddConfigurationInterface
 {
     protected HandlerOptionsNodeDefinition $options;
 
-    public function __construct(protected NodeDefinition $node)
+    public function __construct(protected NodeDefinition|ArrayNodeDefinition|VariableNodeDefinition $node)
     {
         $this->options = new HandlerOptionsNodeDefinition();
     }
